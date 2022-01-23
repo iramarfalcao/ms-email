918b087a24d897880ac895a8476b4f564e10eff3
iramafalcao <iramar.j@hotmail.com>
2022-01-23 10:52:27 -0300

    First commit

---
+++ src/main/kotlin/com/iramarfalcao/email/MsEmailApplication.kt
@@ -0,0 +1,11 @@
+package com.iramarfalcao.email
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsEmailApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsEmailApplication>(*args)
+}
---
+++ src/test/kotlin/com/iramarfalcao/email/MsEmailApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.iramarfalcao.email
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsEmailApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
